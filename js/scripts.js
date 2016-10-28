2b9090bca518af84579677602bf999a1ab2dd9d0
Daniel  Munger <danielmunger@Daniels-MacBook-Pro-2.local>
2016-10-28 09:36:20 -0700

    add javascript function to replace numbers divisible by 15 with 'ping-pong'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 //Back-End: Javascript
 var upperLimit;
+var replaceNumbers;
+var inputedArray = [];
+
+var replaceNumbers = function(limit) {
+  for(i=1; i<=limit; i++){
+    inputedArray.push(i);
+  };
+  for(i=0; i<inputedArray.length; i++){
+    if(inputedArray[i] % 15 === 0){
+      inputedArray[i] = "Ping-Pong";
+      console.log(inputedArray);
+
+    }
+    else{
+
+    }
+  };
+};
+
+
 
 
 
@@ -8,6 +28,7 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     upperLimit = parseInt($("input#limit").val())
-    console.log(upperLimit);
+    replaceNumbers(upperLimit);
+    $("#output").text(upperLimit);
   });
 });
